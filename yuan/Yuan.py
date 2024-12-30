6525c4ba36efd3673513544b77b2285dca2ee602
Cyber-BCat <rayncaine@outlook.com>
2024-12-30 16:06:42 +0800

    Update Yuan.py
    update the mappings

--- yuan/Yuan.py
+++ yuan/Yuan.py
@@ -578,7 +578,7 @@ class imageMinusMask:
 
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique,一行不需要逗号，多行需要逗号
-# 冒号后为函数名
+# 冒号后为函数名即'值' key is in the left of ":", and the value is the function name in the right of the ":".
 NODE_CLASS_MAPPINGS = {
     "Yuan": Yuan_node,
     "Black and white": blackandwhite,
@@ -591,10 +591,11 @@ NODE_CLASS_MAPPINGS = {
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Yuan": "Yuan fake Node",
-    "Black and white": "Yuan Black and White Converter",
+    "Black and white": "Black & White Converter",
     "PhotoShop Transfer": "PS Transfer",
     "Image Judgment": "Image Judgment",
     "ImageMinusMask": "Image Minus Mask",
     "Load Random Images": "Load Random Images",
-    # 冒号后是节点上显示的名字,多行对应多个节点
+    # key is same as the CLASS MAPPINGS, and the value is displayed to the user which is in right of the ":".
+    # 冒号后是节点上显示的名字，即此字典中的‘值’,多行对应多个节点，由jian
 }
