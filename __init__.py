74eb2ce719c43ec3d06e1ba8e9ed24f900ce4528
Cyber-BlackCat <rayncaine@outlook.com>
2025-02-25 23:58:05 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,12 @@
 from .function.LightorDark import lightdarkjudgment
 from .function.mask_morphology import MaskPreprocessMorphology
+from .function.show import ShowSomething, ShowTensorShapeLayout
 from .yuan.MMk import ImageJudgment, imageMinusMask, blackandwhite, PSTransfer, LoadarandomImagefromdir, Number_Decimal
 
 
 # A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique,一行不需要逗号，多行需要逗号
-# 冒号后为函数名即'值' key is in the left of ":", and the value is the function name in the right of the ":".
+# NOTE: names should be globally unique
+
 NODE_CLASS_MAPPINGS = {
     # "a fake Nod": MMk_node,
     "Black and white": blackandwhite,
@@ -16,6 +17,10 @@ NODE_CLASS_MAPPINGS = {
     "Load Random Images": LoadarandomImagefromdir,
     "Mask Preprocess Morphology": MaskPreprocessMorphology,
     "Number": Number_Decimal,
+    "ShowSomething": ShowSomething,
+    "ShowTensorShapeLayout": ShowTensorShapeLayout
+
+    # key is in the left of ":", and the value is the function name in the right of the ":".键左，冒号后为函数名即'值' 
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
@@ -29,6 +34,9 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "Load Random Images": "Load Random Images",
     "Mask Preprocess Morphology": "Mask Preprocess Morphology",
     "Number": "Number",
+    "ShowSomething": "Show Something",
+    "ShowTensorShapeLayout": "Show Tensor"
+    
     # key is same as the CLASS MAPPINGS, and the value is displayed to the user which is in right of the ":".
     # 冒号后是节点上显示的名字，即此字典中的‘值’,多行对应多个节点。
 }
