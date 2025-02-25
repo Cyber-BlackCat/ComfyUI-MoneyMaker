aeedabca8a264cf469cea01924599c42c1dba8cb
Cyber-BlackCat <rayncaine@outlook.com>
2025-02-25 22:37:03 +0800

    Update MMk.py

--- yuan/MMk.py
+++ yuan/MMk.py
@@ -569,19 +569,18 @@ class imageMinusMask:
         image_rgb = np.array(image_rgb).astype(np.float32) / 255.0
         image_rgb = torch.from_numpy(image_rgb)[None,]
 
-        
-       
+               
         return image_rgb,
+
 class Number_Decimal:
     def __init__(self):
         pass
     @classmethod
     def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "number": FLOAT,
-            }
-        }
+        return {"required": 
+                    {"number": ("FLOAT", ),
+                    }
+                }
 
     RETURN_TYPES = ("FLOAT",)
     RETURN_NAMES = ("float",)
@@ -594,26 +593,26 @@ class Number_Decimal:
 
 
 
-# A dictionary that contains all nodes you want to export with their names
-# NOTE: names should be globally unique,一行不需要逗号，多行需要逗号
-# 冒号后为函数名即'值' key is in the left of ":", and the value is the function name in the right of the ":".
-NODE_CLASS_MAPPINGS = {
-    "Yuan": Yuan_node,
-    "Black and white": blackandwhite,
-    "PhotoShop Transfer": PSTransfer,
-    "Image Judgment": ImageJudgment,
-    "ImageMinusMask": imageMinusMask,
-    "Load Random Images": LoadarandomImagefromdir,
-}
+# # A dictionary that contains all nodes you want to export with their names
+# # NOTE: names should be globally unique,一行不需要逗号，多行需要逗号
+# # 冒号后为函数名即'值' key is in the left of ":", and the value is the function name in the right of the ":".
+# NODE_CLASS_MAPPINGS = {
+#     "Yuan": Yuan_node,
+#     "Black and white": blackandwhite,
+#     "PhotoShop Transfer": PSTransfer,
+#     "Image Judgment": ImageJudgment,
+#     "ImageMinusMask": imageMinusMask,
+#     "Load Random Images": LoadarandomImagefromdir,
+# }
 
-# A dictionary that contains the friendly/humanly readable titles for the nodes
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "Yuan": "Yuan fake Node",
-    "Black and white": "Black & White Converter",
-    "PhotoShop Transfer": "PS Transfer",
-    "Image Judgment": "Image Judgment",
-    "ImageMinusMask": "Image Minus Mask",
-    "Load Random Images": "Load Random Images",
-    # key is same as the CLASS MAPPINGS, and the value is displayed to the user which is in right of the ":".
-    # 冒号后是节点上显示的名字，即此字典中的‘值’,多行对应多个节点，由jian
-}
+# # A dictionary that contains the friendly/humanly readable titles for the nodes
+# NODE_DISPLAY_NAME_MAPPINGS = {
+#     "Yuan": "Yuan fake Node",
+#     "Black and white": "Black & White Converter",
+#     "PhotoShop Transfer": "PS Transfer",
+#     "Image Judgment": "Image Judgment",
+#     "ImageMinusMask": "Image Minus Mask",
+#     "Load Random Images": "Load Random Images",
+#     # key is same as the CLASS MAPPINGS, and the value is displayed to the user which is in right of the ":".
+#     # 冒号后是节点上显示的名字，即此字典中的‘值’,多行对应多个节点，由jian
+# }
